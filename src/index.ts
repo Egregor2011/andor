af9dd0058f7c82356e9e8be31c1db0bd30eef5d0
Egregor2011 <egregor2011@gmail.com>
2023-06-10 23:15:00 +0100

    add license

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ type Option<T> = {
   or: Edge<Option<T>, T>;
 };
 
-export const getEvaluation = <T>(graph: Edge<Option<T>, T>): T => {
-  if (typeof graph === "function") {
-    return graph();
+export const getEvaluation = <T>(edge: Edge<Option<T>, T>): T => {
+  if (typeof edge === "function") {
+    return edge();
   }
-  return getEvaluation(graph.case ? graph.and : graph.or);
+  return getEvaluation(edge.case ? edge.and : edge.or);
 };
